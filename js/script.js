24689972631bc6bbcdc855bba7416b9ca3e48556
MoggleJ <99290036+MoggleJ@users.noreply.github.com>
2026-01-30 00:19:58 +0100

    Correcting path

--- js/script.js
+++ js/script.js
@@ -137,7 +137,7 @@ class PortfolioDataLoader {
                 <div class="experience-header">
                     ${exp.logo ? `
                         <div class="company-logo">
-                            <img src="./images/${exp.logo}" alt="${exp.company}">
+                            <img src="./Portfolio_2026/images/${exp.logo}" alt="${exp.company}">
                         </div>
                     ` : `
                         <div class="company-logo">
